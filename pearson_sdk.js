91e1d707e3c646e3cf5d3960fb7f5a9fa975a004
Lawrie Nichols <lawrie.nichols@pearson.com>
2013-11-04 09:41:28 +0000

    Simplified search. Added getById

--- pearson_sdk.js
+++ pearson_sdk.js
@@ -1,15 +1,6 @@
 // Pearson Top Ten Travel API wrapper. V0.9
 // Base Url for API call http://api.pearson.com/v2/
 
-/// BASE OBJECT
-function Pearson(apikey) {
-    'use strict';
-    this.apikey = apikey;
-    this.base = "http://api.pearson.com/v2/"
-    console.log("HAL 9000 says don't work weekends Lawrie")
-    return this;
-};
-
 var Api = {
     dictionaries: function(apikey) { 
         return new Pearson(apikey).dictionaries(); 
@@ -22,21 +13,14 @@ var Api = {
     }
  };
 
-function Endpoint(pearson,path) {
-    this.pearson = pearson;
-    this.path = path; //Set by the dot method??
-};
-
-Endpoint.prototype.getById = function(Id) {
-    this.pearson.getById(this.path,Id)
+/// BASE OBJECT
+function Pearson(apikey) {
+    'use strict';
+    this.apikey = apikey;
+    this.base = "http://api.pearson.com/v2/"
+    console.log("HAL 9000 says don't work weekends Lawrie")
     return this;
 };
-
-Endpoint.prototype.search = function(json, offset, limit) {
-    this.pearson.search(json, offset, limit);
-    return this;
-};
-
 
 Pearson.prototype.dictionaries = function() {
     'use strict';
@@ -53,19 +37,22 @@ Pearson.prototype.travel = function() {
 
 };
 
-Pearson.prototype.getById = function(path,id) {
-    console.log("Calling on ",path,id)
+// Endpoint(s)
+function Endpoint(pearson,path) {
+    this.pearson = pearson;
+    this.path = path; //Set by the dot method??
 };
 
+Endpoint.prototype.getById = function(Id) {
+    this.pearson.getById(this.path,Id)
+    return this;
+};
 
 Endpoint.prototype.search = function(json, offset, limit){
     var query;
 
-     limit = limit || 10;
-     offset = offset || 0;
-
-     json.limit = limit;
-     json.offset = offset;
+    json.limit = limit || 10;
+    json.offset = offset || 0;
 
      if (typeof this.pearson.apikey === "undefined") {
         query = $.param(json);
@@ -76,39 +63,20 @@ Endpoint.prototype.search = function(json, offset, limit){
         this.query = query;
      };
 
-     console.log(this);
-     console.log(query);
-     this.query = query;
-     var ex = constructUrl(this);
-     return ex;
+     var fullUrl = this.pearson.url + this.path + "?" + query;
+
+    return grab(fullUrl);
 };
 
-// Bring me a specific result by Id from the list 
-function getById(id) {////////// WIP 
+Endpoint.prototype.getById = function(Id) {
     'use strict';
-    var results;
-    var z;
-    if (typeof this.pearson.apikey === "undefined"){
-        z = this.url;
-        results = grab(z);
-        } else { 
-        z = this.url + "?" + this.pearson.apikey.substr(1);
-         results = grab(z); 
+
+    var fullUrl = this.pearson.url + this.path + "/"+Id;
+
+    if (typeof this.pearson.apikey === "undefined") {
+        fullUrl = fullUrl + "?apikey="+this.pearson.apikey
     }
-         return results;
-};
-
-// Build url inc search term - apikey addition is taken care of by search.
-function constructUrl(obj) {
-    var fullUrl;
-    console.log("URL= ", obj);
-    fullUrl = obj.pearson.url + obj.path + "?" + obj.query;
-
-    // insert fetch method here
-     grab(fullUrl);
-
-     return result;
-
+    return grab(fullUrl);
 };
 
 // Generic get me stuff from a url method
