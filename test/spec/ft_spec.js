c66df741a33ad453357450d2e7cdced97ab8b604
Toby Sims <toby.sims@gmail.com>
2013-11-11 12:41:50 +0000

    added ft spec

---
+++ test/spec/ft_spec.js
@@ -0,0 +1,26 @@
+describe("The FT Articles Api", function(){
+	var ft = PearsonApis.ftarticles("JZNt3YM1veh1d6HDiCpA86vFJvuRefjw");
+	var art = ft.articles;
+
+	it("should have the same base url", function(){
+		expect(ft.base).toEqual("http://api.pearson.com/v2/")
+	});
+
+	it("should have the Endpoint of articles available to it", function(){
+		expect(ft.articles).toBeDefined();
+	});
+
+	it("should be able to return articles", function(){
+		var search = {search: "mclaren"};
+		var mclaren = art.search(search);
+		expect(mclaren.status).toEqual(200);
+	});
+
+	it("should be able to return a recipe by id", function(){
+		var mcx = art.getById("cqDYjsGQbj");
+		expect(mcx.status).toEqual(200);
+		expect(mcx.result).toBeDefined();
+	});
+
+
+});
